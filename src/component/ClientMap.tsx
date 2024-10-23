e27fbf73f3da91b59fe755437ee70d408a9f6de6
anurag <anurag.guptacs2020@indoreinstitute.com>
2024-10-23 14:03:30 +0530

    complete working

--- src/component/ClientMap.tsx
+++ src/component/ClientMap.tsx
@@ -8,9 +8,19 @@ import L from 'leaflet';
 // Fix for default marker icon
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/marker-icon-2x.png',
-  iconUrl: '/marker-icon.png',
-  shadowUrl: '/marker-shadow.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
+const customIcon = new L.Icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
 });
 
 interface ClientMapProps {
@@ -26,7 +36,7 @@ function MapUpdater({ coordinates }: ClientMapProps) {
     }
   }, [coordinates, map]);
 
-  return coordinates ? <Marker position={coordinates} /> : null;
+  return coordinates ? <Marker position={coordinates} icon={customIcon} /> : null;
 }
 
 export default function ClientMap({ coordinates }: ClientMapProps) {
